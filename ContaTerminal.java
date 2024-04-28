dcad9b8b175a989d6372446d6b19b73a29f5a9b7
wilian camilo <wiliancamilos1@gmail.com>
2024-04-27 21:07:07 -0300

    inicio do projeto, capturando dados iniciais do cliente

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -5,21 +5,21 @@ public class ContaTerminal {
 
     public static void main (String[] args){
        
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("Olá, como posso te chamar? ");
+            String NOME = scanner.next();
 
-        System.out.println("Olá, como posso te chamar? ");
-        String NOME = scanner.next();
+            System.out.println("Qual sua Agencia de preferencia? ");
+            String AGENCIA = scanner.next();
 
-        System.out.println("Qual sua Agencia de preferencia? ");
-        String AGENCIA = scanner.next();
+            System.out.println("Digite o numero de sua conta: ");
+            String conta = scanner.next();
 
-        System.out.println("Digite o numero de sua conta: ");
-        String conta = scanner.next();
+            System.out.println("Qual vai ser seu deposito (Saldo) inicial? ");
+            String saldo = scanner.next();
 
-        System.out.println("Qual vai ser seu deposito (Saldo) inicial? ");
-        String saldo = scanner.next();
-
-        System.out.println("Olá, " + NOME + "! Obrigado por criar sua conta junto ao nosso banco, sua agencia é " + AGENCIA + ", e conta de numero " + conta + ", ja seu saldo atual é R$: " + saldo + " . Lembrando que ele já esta disponivel para saques!");
+            System.out.println("Olá, " + NOME + "! Obrigado por criar sua conta junto ao nosso banco, sua agencia é " + AGENCIA + ", e conta de numero " + conta + ", ja seu saldo atual é R$: " + saldo + " . Lembrando que ele já esta disponivel para saques!");
+        }
 
     }
 }
